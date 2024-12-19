25ab1c6f2edabe52c7ea750c91ed57fe89cfed7f
Rose <cool5tarXV@gmail.com>
2024-12-19 20:00:27 +0800

    Update bigmaac.h

--- bigmaac.h
+++ bigmaac.h
@@ -4,4 +4,9 @@
 #define DEFAULT_MAX_BIGMAAC (1024L*1024*1024*512) //512GB
 #define DEFAULT_MAX_FRIES (1024L*1024*1024*512) //512GB
 #define DEFAULT_FRY_SIZE_MULTIPLE 256
+void *malloc(size_t size);
+void *calloc(size_t count, size_t size);
+void *realloc(void * ptr, size_t size);
+void *reallocarray(void * ptr, size_t size,size_t count);
+void free(void* ptr);
 
